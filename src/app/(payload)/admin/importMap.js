d82ec0f861c011286f47ece46c707ec8bad8ea3b
rohansharp <rohansharp@gmail.com>
2026-01-18 19:35:35 +1100

    chore: regenerate Payload importMap for new navigation components

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -2,22 +2,44 @@ import { LoanAccountServicing as LoanAccountServicing_ddd43f8b2788d92f39012f5e3f
 import { NotificationAction as NotificationAction_ff7594e27eaf34e7ce9acae213b05415 } from '@/components/Notifications/NotificationAction'
 import { NavSearchTrigger as NavSearchTrigger_aef8b4b7b9afc7097ff45e7590131c08 } from '@/components/navigation/NavSearchTrigger'
 import { NavDashboardLink as NavDashboardLink_8ed26f5dfcf4b8f60da98c3488976ae8 } from '@/components/navigation/NavDashboardLink'
+import { NavCollectionsLink as NavCollectionsLink_cace573cc395aa45fa1be624b4874bcd } from '@/components/navigation/NavCollectionsLink'
 import { NavApprovalsLink as NavApprovalsLink_432895ce587e4825ef60ac4a4de8aa43 } from '@/components/navigation/NavApprovalsLink'
+import { NavPeriodCloseLink as NavPeriodCloseLink_b876b3abd416c3d860c4802b5c1185f1 } from '@/components/navigation/NavPeriodCloseLink'
+import { NavECLConfigLink as NavECLConfigLink_3ff0f20809012ed106da89f9feaf716c } from '@/components/navigation/NavECLConfigLink'
+import { NavExportsLink as NavExportsLink_30210b8f1b8d6622c22618d359a47328 } from '@/components/navigation/NavExportsLink'
+import { NavInvestigationLink as NavInvestigationLink_25d4a0e46dfdaebb4b30fe5c09ac4503 } from '@/components/navigation/NavInvestigationLink'
 import { default as default_2aef381f6a0e89088ab303e1162933ca } from '@/providers'
 import { DashboardViewWithTemplate as DashboardViewWithTemplate_aa38ffaef1440457ff25f89c94f88b6b } from '@/components/DashboardView/DashboardViewWithTemplate'
 import { ServicingViewWithTemplate as ServicingViewWithTemplate_04dbd51012de8c56c1deb3ec9516015c } from '@/components/ServicingView/ServicingViewWithTemplate'
 import { ApprovalsViewWithTemplate as ApprovalsViewWithTemplate_03ad776ce26465d586697fa5d953dd80 } from '@/components/ApprovalsView/ApprovalsViewWithTemplate'
 import { MyActivityViewWithTemplate as MyActivityViewWithTemplate_59e3ffb5a5d6849cbc82cab26e75164b } from '@/components/MyActivityView/MyActivityViewWithTemplate'
+import { CollectionsViewWithTemplate as CollectionsViewWithTemplate_6ff107173ce80a76a0f8a31dcf506def } from '@/components/CollectionsView/CollectionsViewWithTemplate'
+import { SystemStatusViewWithTemplate as SystemStatusViewWithTemplate_ec75aba93ca5497fc28565b2d315a0bb } from '@/components/SystemStatusView/SystemStatusViewWithTemplate'
+import { PeriodCloseViewWithTemplate as PeriodCloseViewWithTemplate_5066c3164c49a10261fae829cb52078b } from '@/components/PeriodCloseView/PeriodCloseViewWithTemplate'
+import { ECLConfigViewWithTemplate as ECLConfigViewWithTemplate_da54698be16fc6d457187852eba8f854 } from '@/components/ECLConfigView/ECLConfigViewWithTemplate'
+import { ExportCenterViewWithTemplate as ExportCenterViewWithTemplate_df012e52e067a6044c3439dd142ed735 } from '@/components/ExportCenterView/ExportCenterViewWithTemplate'
+import { InvestigationViewWithTemplate as InvestigationViewWithTemplate_da95a625101a2d97fc47681066b86998 } from '@/components/InvestigationView/InvestigationViewWithTemplate'
 
 export const importMap = {
   "@/components/LoanAccountServicing#LoanAccountServicing": LoanAccountServicing_ddd43f8b2788d92f39012f5e3f407ad6,
   "@/components/Notifications/NotificationAction#NotificationAction": NotificationAction_ff7594e27eaf34e7ce9acae213b05415,
   "@/components/navigation/NavSearchTrigger#NavSearchTrigger": NavSearchTrigger_aef8b4b7b9afc7097ff45e7590131c08,
   "@/components/navigation/NavDashboardLink#NavDashboardLink": NavDashboardLink_8ed26f5dfcf4b8f60da98c3488976ae8,
+  "@/components/navigation/NavCollectionsLink#NavCollectionsLink": NavCollectionsLink_cace573cc395aa45fa1be624b4874bcd,
   "@/components/navigation/NavApprovalsLink#NavApprovalsLink": NavApprovalsLink_432895ce587e4825ef60ac4a4de8aa43,
+  "@/components/navigation/NavPeriodCloseLink#NavPeriodCloseLink": NavPeriodCloseLink_b876b3abd416c3d860c4802b5c1185f1,
+  "@/components/navigation/NavECLConfigLink#NavECLConfigLink": NavECLConfigLink_3ff0f20809012ed106da89f9feaf716c,
+  "@/components/navigation/NavExportsLink#NavExportsLink": NavExportsLink_30210b8f1b8d6622c22618d359a47328,
+  "@/components/navigation/NavInvestigationLink#NavInvestigationLink": NavInvestigationLink_25d4a0e46dfdaebb4b30fe5c09ac4503,
   "@/providers#default": default_2aef381f6a0e89088ab303e1162933ca,
   "@/components/DashboardView/DashboardViewWithTemplate#DashboardViewWithTemplate": DashboardViewWithTemplate_aa38ffaef1440457ff25f89c94f88b6b,
   "@/components/ServicingView/ServicingViewWithTemplate#ServicingViewWithTemplate": ServicingViewWithTemplate_04dbd51012de8c56c1deb3ec9516015c,
   "@/components/ApprovalsView/ApprovalsViewWithTemplate#ApprovalsViewWithTemplate": ApprovalsViewWithTemplate_03ad776ce26465d586697fa5d953dd80,
-  "@/components/MyActivityView/MyActivityViewWithTemplate#MyActivityViewWithTemplate": MyActivityViewWithTemplate_59e3ffb5a5d6849cbc82cab26e75164b
+  "@/components/MyActivityView/MyActivityViewWithTemplate#MyActivityViewWithTemplate": MyActivityViewWithTemplate_59e3ffb5a5d6849cbc82cab26e75164b,
+  "@/components/CollectionsView/CollectionsViewWithTemplate#CollectionsViewWithTemplate": CollectionsViewWithTemplate_6ff107173ce80a76a0f8a31dcf506def,
+  "@/components/SystemStatusView/SystemStatusViewWithTemplate#SystemStatusViewWithTemplate": SystemStatusViewWithTemplate_ec75aba93ca5497fc28565b2d315a0bb,
+  "@/components/PeriodCloseView/PeriodCloseViewWithTemplate#PeriodCloseViewWithTemplate": PeriodCloseViewWithTemplate_5066c3164c49a10261fae829cb52078b,
+  "@/components/ECLConfigView/ECLConfigViewWithTemplate#ECLConfigViewWithTemplate": ECLConfigViewWithTemplate_da54698be16fc6d457187852eba8f854,
+  "@/components/ExportCenterView/ExportCenterViewWithTemplate#ExportCenterViewWithTemplate": ExportCenterViewWithTemplate_df012e52e067a6044c3439dd142ed735,
+  "@/components/InvestigationView/InvestigationViewWithTemplate#InvestigationViewWithTemplate": InvestigationViewWithTemplate_da95a625101a2d97fc47681066b86998
 }
